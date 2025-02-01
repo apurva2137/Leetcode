67bff65c4526c7b572ad0accfee100063e1af6bc
Apurva_1131 <158991125+apurva2137@users.noreply.github.com>
2025-02-01 23:14:24 +0530

    Time: 1 ms (25.88%), Space: 12.1 MB (10.26%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+       unordered_map<int,int> mp;
+       for(int i=0;i<arr.size();i++){
+        mp[arr[i]]++;
+       }
+       set<int> s;
+       for(auto& it: mp){
+        s.insert(it.second);
+       }
+       return mp.size()==s.size();
+    }
+};
