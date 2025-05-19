2b9fde7cb68f23c9690a5b35cf44d7aab964fed3
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-05-19 12:21:39 +0530

    Time: 0 ms (100%), Space: 22.7 MB (99%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -7,11 +7,9 @@ public:
         int b= nums[1];
         int c=nums[2];
         if(a+b <= c) return "none";
-        for(int i=0; i<3; i++){
-            if(nums[i]==nums[i+1]) equal++;
-        }
-        if(equal==3) return "equilateral";
-        else if(equal==2) return "isosceles";
+       
+        if(a==b && b==c) return "equilateral";
+        else if(a==b || b==c || a==c) return "isosceles";
         else  return "scalene";
     }
 };
