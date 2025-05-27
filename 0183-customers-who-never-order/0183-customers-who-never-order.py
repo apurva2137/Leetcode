604137f51c25dc750b3e8f4a9e6efb1bf6e8b057
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-05-27 15:14:03 +0530

    Time: 284 ms (53.74%), Space: 68.1 MB (20.18%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    buyers = customers.merge(orders , how = 'left' , left_on = 'id' , right_on='customerId')
 
-    df=customers[~customers['id'].isin(orders['customerId'])]
-    df=df[['name']].rename(columns={'name':'Customers'})
-    return df
+    non_buyers = buyers[buyers['customerId'].isna()]
+
+    return non_buyers[['name']].rename(columns ={'name' : 'Customers'})
     
