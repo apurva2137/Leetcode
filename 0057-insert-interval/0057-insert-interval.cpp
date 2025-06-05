5160be65e66acde4ca3585e5ad8bf51c40459823
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-06-05 19:30:40 +0530

    Time: 1 ms (38.23%), Space: 21.7 MB (74.19%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& itv, vector<int>& newitv) {
+        vector<vector<int>> ans;
+        int i = 0 ;
+        int n = itv.size();
+        while(i < n && itv[i][1] < newitv[0]){
+            ans.push_back(itv[i]);
+            i++;
+        }
+
+        while(i < n && itv[i][0] <= newitv[1]){
+            newitv[0] = min(itv[i][0] , newitv[0]);
+            newitv[1] = max(itv[i][1] , newitv[1]);
+            i++;
+        }
+        ans.push_back(newitv);
+       while(i < n){
+            ans.push_back(itv[i]);
+            i++;
+        }
+        return ans;
+    }
+};
