0ff715da285e64a35c5373d6a01599863fbaf37e
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-06-16 15:44:37 +0530

    Time: 10 ms (48.43%), Space: 29.7 MB (90.89%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
+    int numEnclaves(vector<vector<int>>& board) {
+       int m = board.size();
+        int n = board[0].size();
         queue<pair<int,int>> q;
-        
-        for(int i=0;i<n;i++){
-           if(grid[i][0]==1) q.push({i,0});
-           if(grid[i][m-1]==1) q.push({i,m-1});
+
+        for(int i = 0 ; i < m ; i++){
+            if(board[i][0] == 1) q.push({i,0});
+            if(board[i][n-1] == 1) q.push({i,n-1});
         }
-        for(int j=0;j<m;j++){
-           if(grid[0][j]==1) q.push({0,j});
-           if(grid[n-1][j]==1) q.push({n-1,j});
+        for(int j = 0 ; j < n ; j++){
+            if(board[0][j] == 1) q.push({0,j});
+            if(board[m-1][j] == 1) q.push({m-1,j});
         }
-        int count=0;
+        int row[4] = {-1, 0 , 1 , 0};
+        int col[4] = {0, -1 , 0 , 1};
+
         while(!q.empty()){
-             int r=q.front().first;
-             int c=q.front().second;
-             q.pop();
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
 
-             int row[4]={1,0,-1,0};
-             int col[4]={0,-1,0,1};
-             if(grid[r][c]!=1) continue;
-             grid[r][c]=0;
-             for(int i=0;i<4;i++){
-                int nr= r+row[i];
-                int nc= c+col[i];
+            if(board[r][c]!=1) continue;
+            board[r][c] = 0;
 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+            for(int i = 0 ; i < 4 ; i++){
+                int nr = r + row[i];
+                int nc = c + col[i];
+
+                if(nr >= 0 && nr < m &&
+                 nc >= 0 && nc < n && board[nr][nc] ==1 ){
                     q.push({nr,nc});
                 }
-             }
+            } 
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1) count++;
+        int count = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j <  n ; j++){
+              if(board[i][j] == 1) count++;
             }
         }
         return count;
