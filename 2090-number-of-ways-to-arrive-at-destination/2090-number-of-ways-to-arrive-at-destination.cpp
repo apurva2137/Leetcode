724c495e204089ab55612a59f43b23df3bddaad0
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-06-18 15:11:39 +0530

    Time: 4 ms (83.72%), Space: 35.1 MB (74.91%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -2,39 +2,41 @@ class Solution {
     int mod = 1e9 + 7;
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> graph(n);
-        
-        for(auto& road : roads){
-            int u = road[0], v = road[1], wt = road[2];
-            graph[u].push_back({v, wt});
-            graph[v].push_back({u, wt});
+       vector<vector<pair<int,int>>> graph(n);
+
+       for(int i = 0 ; i < roads.size() ; i++){
+            int src = roads[i][0];
+            int node = roads[i][1];
+            int wt = roads[i][2];
+            graph[src].push_back({node,wt});
+            graph[node].push_back({src,wt});
         }
+
+       priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
         
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
-        vector<long long> dist(n, LLONG_MAX);
-        vector<int> ways(n, 0);
-        
-        dist[0] = 0;
+        pq.push({0,0});
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> ways(n,0);
         ways[0] = 1;
-        pq.push({0, 0});
-        
+        dist[0] = 0;
+
+
         while(!pq.empty()){
             long long dis = pq.top().first;
             int node = pq.top().second;
             pq.pop();
             
-            for(auto& [adjNode, wt] : graph[node]){
+            for(auto& [adjNode , wt]: graph[node]){
                 if(dis + wt < dist[adjNode]){
                     dist[adjNode] = dis + wt;
                     ways[adjNode] = ways[node];
-                    pq.push({dist[adjNode], adjNode});
+                    pq.push({dist[adjNode] , adjNode});
                 }
-                else if(dis + wt == dist[adjNode]){
+                else if(dis + wt == dist[adjNode] ){
                     ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
                 }
             }
         }
-        
         return ways[n-1];
     }
 };
