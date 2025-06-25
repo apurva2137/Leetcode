be118cbf11c0473e6120b6c457d0c54edee49b69
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-06-26 00:57:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62.68%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int sum=0;
-        for(int i=0;i<s.size()-1;i++){
-            sum+=abs((int)s[i]-(int)s[i+1]);
+        int score = 0; 
+        for(int i = 1 ; i < s.size() ; i++){
+            score += abs((s[i] + '0') - (s[i-1] + '0'));
         }
-        return sum;
+        return score;
     }
 };
