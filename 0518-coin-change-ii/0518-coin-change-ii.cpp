eb52a12afdd9a23eb3a332536b252ee00d357e70
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-07-22 01:03:55 +0530

    Time: 31 ms (26.04%), Space: 28.6 MB (35.1%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subset(int i , int amount , vector<int>& coins , vector<vector<int>>& dp){
+        int n = coins.size();
+        if(i>=n) return 0;
+        if(amount == 0) return 1;
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int take = 0 ;
+        if(coins[i] <= amount){
+            take = subset(i , amount - coins[i] , coins , dp);
+        }
+        int not_take = subset(i+1 , amount , coins , dp);
+        return dp[i][amount] = (take + not_take) ;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        sort(coins.begin(),coins.end());
+        vector<vector<int>> dp(n+1 , vector<int>(amount + 1 , -1));
+        return subset(0 , amount, coins , dp);
+    }
+};
