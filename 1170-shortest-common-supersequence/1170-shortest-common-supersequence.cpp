b17dcd7de0d5c032e0b5dbe3580e4f55c74a3651
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-07-27 00:57:38 +0530

    Time: 23 ms (88.87%), Space: 31.2 MB (6.43%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
-     string lcs(string s , string t){
+    string lcs(string s , string t){
         int m = s.size();
         int n = t.size();
 
         vector<vector<int>> dp(m+1 , vector<int>(n+1,0));
 
-        for(int i = 1 ; i<=m ; i++){
-            for(int j = 1 ; j<=n ; j++){
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1 ; j <= n ; j++){
                 if(s[i-1] == t[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
                 else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
             }
         }
+
         int i = m;
         int j = n;
-        string ans="";
-        while(i>0 && j >0){
-            if(s[i-1]==t[j-1]){
-                ans += s[i-1];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]) {
-                ans += s[i-1];
-                i--;
-            }
-            else{
-                ans += t[j-1];
-                j--;
-            }
 
+        string ans = "";
+
+        while(i > 0 && j > 0){
+             if(s[i-1] == t[j-1]){
+                ans = s[i-1] + ans;
+                i--;
+                j--;
+             }
+             else if(dp[i-1][j] > dp[i][j-1]){
+                ans = s[i-1] + ans;
+                i--;
+             }
+             else{
+                ans = t[j-1] + ans;
+                j--;
+             }
         }
-         while (i > 0) {
-            ans += s[i - 1];
+        while(i > 0){
+            ans = s[i-1] + ans;
             i--;
         }
-        while (j > 0) {
-            ans += t[j - 1];
+        while(j > 0){
+            ans = t[j-1] + ans;
             j--;
         }
-        reverse(ans.begin() , ans.end());
         return ans;
     }
-    string shortestCommonSupersequence(string s, string t) {
-        return lcs(s,t);
+    string shortestCommonSupersequence(string str1, string str2) {
+        return lcs(str1,str2);
     }
 };
