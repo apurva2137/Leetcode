64e98c37e941814d27b508b08eb9777cdb7b256e
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-07-27 13:18:15 +0530

    Time: 31 ms (35.62%), Space: 141.3 MB (26.06%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,10 +5,13 @@ public:
         vector<int>res;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            while(!dq.empty() && dq.front().first<=(i-k)) dq.pop_front();
-            while(!dq.empty() && dq.back().second<=nums[i]) dq.pop_back();
-            dq.push_back({i,nums[i]});
-            if(i>=k-1) res.push_back(dq.front().second);
+            while(!dq.empty() && dq.front().first <= (i-k)) dq.pop_front();
+
+            while(!dq.empty() && dq.back().second <= nums[i]) dq.pop_back();
+
+            dq.push_back({i , nums[i]});
+
+            if( i >= k-1) res.push_back(dq.front().second);
         }
         return res;
     }
