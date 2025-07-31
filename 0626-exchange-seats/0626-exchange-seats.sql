50f1d88741a8df3edacf4e37f1ecc16e380ed4df
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-01 00:49:34 +0530

    Time: 316 ms (88.45%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,5 @@
-SELECT 
-    id,
-    CASE
-        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
-        ELSE LEAD(student,1,student) OVER(ORDER BY id)
-    END AS student
-FROM Seat;
+# Write your MySQL query statement below
+select s1.id , coalesce(s2.student , s1.student ) as student
+from seat s1
+left join seat s2 on 
+(s1.id % 2 = 1 and s2.id = s1.id + 1) or (s1.id % 2 = 0 and s2.id = s1.id -1 )
