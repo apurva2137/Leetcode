821fbcd107fc69727847a01bc40a58e690872fd2
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-02 00:17:59 +0530

    Time: 4 ms (68.43%), Space: 20.1 MB (31.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& edges) {
-        vector<int> indegree(V,0);
-        vector<vector<int>> adj(V);
-        for(auto& edge: edges){
+    bool canFinish(int n, vector<vector<int>>& prereq) {
+        vector<int> indeg(n);
+        vector<vector<int>> adj(n);
+
+        for(auto edge : prereq){
             adj[edge[1]].push_back(edge[0]);
-            indegree[edge[0]]++;
+            indeg[edge[0]]++;
         }
-       
+        
         queue<int> q;
-        for(int i=0;i<indegree.size();i++){
-            if(indegree[i]==0) q.push(i);
+        for(int i = 0 ; i < indeg.size() ; i++){
+            if(indeg[i] == 0) q.push(i);
         }
         vector<int> topo;
         while(!q.empty()){
-            int node=q.front();
+            int node = q.front();
             q.pop();
-            
+
             for(auto it: adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+                indeg[it]--;
+                if(indeg[it] == 0) q.push(it);
             }
             topo.push_back(node);
         }
-        return (topo.size()==V);
+        return topo.size() == n;
     }
 };
