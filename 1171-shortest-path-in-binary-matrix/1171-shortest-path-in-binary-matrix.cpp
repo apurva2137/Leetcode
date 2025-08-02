9c782499649819caa21277cad1e2e1d10173ec62
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-02 15:43:31 +0530

    Time: 15 ms (77.51%), Space: 26.3 MB (30.37%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,37 +3,40 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
 
         queue<pair<int,pair<int,int>>> q;
         vector<vector<int>> dist(m , vector<int>(n , INT_MAX));
-        
-        if (grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
 
-        q.push({0,{0,0}});
+        q.push({0 , { 0 , 0 }});
+        dist[0][0] = 0;
 
         int row[8] = {-1 , 0 , 1 , 0 , -1 , 1 , -1 , 1};
         int col[8] = {0, -1 , 0 , 1 , -1 , 1 , 1 , -1};
 
-        dist[0][0] = 0;
         while(!q.empty()){
+
             int dis = q.front().first;
             int r = q.front().second.first;
             int c = q.front().second.second;
             q.pop();
+
             if(r == m-1 && c == n-1) return dis+1;
+
             for(int i = 0 ; i < 8 ; i++){
                 int nr = r + row[i];
                 int nc = c + col[i];
 
-                if(nr >= 0 && nr < m && 
-                   nc >= 0 && nc < n &&
-                   grid[nr][nc] == 0 && dis+1 < dist[nr][nc])
-                   {
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                   grid[nr][nc] == 0 && dist[nr][nc] > dis + 1){
+                    
                     dist[nr][nc] = dis + 1;
                     q.push({dis+1 , {nr,nc}});
-                   }
+
+                 }
             }
         }
-        return -1;
+       return -1;
     }
 };
