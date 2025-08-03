4786b715c0dcaf22cf623f23c4efdebfe48f18ab
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-03 12:28:22 +0530

    Time: 54 ms (56.68%), Space: 24.4 MB (90.76%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
-
-        priority_queue<
+       priority_queue<
                 pair<int, pair<int, int>>,              
                 vector<pair<int, pair<int, int>>>,      
                 greater<pair<int, pair<int, int>>>     
             > pq;
 
+        int m = heights.size();
+        int n = heights[0].size();
+
         vector<vector<int>> dis(m , vector<int>(n,INT_MAX));
 
         pq.push({0,{0,0}});
+        dis[0][0] = 0;
         
-        int row[4] = {-1 , 0 , 1 , 0};
-        int col[4] = {0 , -1 , 0 , 1};
+        int row[4] ={-1,1,0,0};
+
+        int col[4] ={0,0,-1,1};
 
         while(!pq.empty()){
+
             int diff = pq.top().first;
+
             int r = pq.top().second.first;
             int c = pq.top().second.second;
+
             pq.pop();
 
             if(r == m-1 && c == n-1) return diff;
 
-            for(int i = 0 ; i < 4 ; i++){
+            for(int i = 0 ; i < 4; i++){
                 int nr = r + row[i];
                 int nc = c + col[i];
 
-                if(nr >=0 && nr < m && nc >= 0 && nc < n ){
-                    int effort = abs(heights[nr][nc] - heights[r][c]);
-                    int newdiff = max(effort, diff);
-                    if(newdiff < dis[nr][nc]){
-                        dis[nr][nc] = newdiff;
-                        pq.push({newdiff,{nr,nc}});
-                    }
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n){
+                   int absdiff = abs(heights[nr][nc] - heights[r][c]);
+                   int maxdiff = max(diff , absdiff);
+
+                   if(maxdiff < dis[nr][nc]){
+                      dis[nr][nc] = maxdiff;
+                      pq.push({maxdiff , {nr,nc}});
+                   }
                 }
-            }
+            } 
         }
         return -1;
     }
