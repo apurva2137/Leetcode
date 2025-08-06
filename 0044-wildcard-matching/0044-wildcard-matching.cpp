3207e2bfc583e590b33abd7f45780c77d0ad640a
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-06 12:49:39 +0530

    Time: 43 ms (12.96%), Space: 35.8 MB (5.41%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool isAllStars(string&s,int n){
-        for(int i = 1; i <= n; i++){
-            if(s[i-1] != '*') return false; 
+    bool isallstars(int i , string p ){
+       for(int k = 1 ; k <= i ; k++){
+            if(p[k-1] != '*') return false;
         }
-            return true; 
+        return true;
     }
-    bool isMatch(string s2, string s1) {
-        int n = s1.size(); 
-        int m = s2.size(); 
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false)); 
-        dp[0][0] = true; 
-        for (int j = 1; j <= m; j++) {
-            dp[0][j] = false;
-        }
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = isAllStars(s1, i);
-        }
+  
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j<= m; j++){
-                if(s1[i-1] == s2[j-1] || s1[i-1] == '?'){
+        vector<vector<int>> dp(m+1 , vector<int>(n+1,false));
+        dp[0][0] = true;
+
+        for(int j = 1 ; j <= n ; j++){
+            dp[0][j] = isallstars(j,p);
+        }
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j<= n; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
                     dp[i][j] = dp[i-1][j-1]; 
-                }else if(s1[i-1] == '*'){
+                }else if(p[j-1] == '*'){
                     dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }else{
                     dp[i][j] = false; 
                 }
             }
         }
-
-        return dp[n][m]; 
+        return dp[m][n];
     }
 };
