7bb757fd5ff4fabb1f72497a8c3aea3de1bff3bd
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-18 17:20:43 +0530

    Time: 8 ms (59.08%), Space: 23 MB (11.85%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    long long countb(vector<int>& piles, int hour){
-        long long total = 0;
+    long long hourly(vector<int>& piles, int h){
+        long long tot = 0;
         for(int i = 0 ; i < piles.size() ; i++){
-            total += ceil(double(piles[i]) / double(hour));
+            tot += ceil(double(piles[i]) / double(h));
         }
-        return total;
+        return tot;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1;
         int right = *max_element(piles.begin() , piles.end());
         int ans = right;
         while(left <= right){
-            int mid = left + ((right - left)/2);
-            if(countb(piles , mid) <= h){
+            int mid = left + ((right - left) / 2);
+            if(hourly(piles,mid) <= h){
                 ans = mid;
-                right = mid -1;
+                right = mid-1;
             }
             else left = mid + 1;
         }
