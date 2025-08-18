37a6d13e6de63bbf5d21e848770d0c2ad1167de2
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-19 01:03:49 +0530

    Time: 0 ms (100%), Space: 8.8 MB (70.21%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -7,27 +7,21 @@ public:
         int i = 0; 
         int j = 0; 
 
-        while (i < s.size() && j < p.size()) {
+        while( j < p.size() && i < s.size()){
             string word = "";
 
-            while (i < s.size() && s[i] != ' ') {
+            while(i < s.size() && s[i]!=' ' ){
                 word += s[i];
                 i++;
             }
             i++;
-
-            if (mp1.count(p[j]) && mp1[p[j]] != word || 
-                mp2.count(word) && mp2[word] != p[j]) {
-                return false;
-            }
-
-            mp1[p[j]] = word;
+            if(mp2.count(word) && mp2[word] != p[j] ||
+             mp1.count(p[j]) && mp1[p[j]] != word) return false;
+            mp1[p[j]]=word;
             mp2[word] = p[j];
             j++;
-
-            if (j == p.size() && i < s.size()) return false;
+            if(i < s.size() && j >= p.size()) return false;
         }
-
-        return j == p.size() && i >= s.size();
+        return i>=s.size() && j== p.size() ;
     }
 };
