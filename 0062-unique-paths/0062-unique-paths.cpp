cf04f56fadfc430678e5930a44268982c07d7f30
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-22 17:48:49 +0530

    Time: 2 ms (18.49%), Space: 9.4 MB (8.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,17 @@
 class Solution {
-private:
-    int path(int m, int n, int i, int j, vector<vector<int>>& dp){
-        if(i== (m-1) && j==(n-1)) {
-            return 1;
-        }
-        
-        if(i>=m) return 0;
-        if(j>=n) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int right = path(m,n,i,j+1,dp);
-        int bottom = path(m,n,i+1,j,dp);
-
-        return  dp[i][j]=right+bottom;
-
-    }
 public:
+    int grid(int i , int j , int m , int n,vector<vector<int>>& dp){
+        if( i >= m || j >= n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int right = grid(i,j+1,m,n,dp);
+        int bottom = grid(i+1,j,m,n,dp);
+
+        return dp[i][j] = right+bottom;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return path(m,n,0,0,dp);
-        
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return grid(0,0,m,n,dp);
     }
 };
