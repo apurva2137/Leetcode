6a3be28e469f892fcdb210430b27a3cbd2a2de23
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-23 23:31:08 +0530

    Time: 31 ms (26.41%), Space: 28.7 MB (35.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int subset(int i , int amount , vector<int>& coins , vector<vector<int>>& dp){
-        int n = coins.size();
-        if(i>=n) return 0;
+    int cntchange(int i , int amount , vector<int>& coins , vector<vector<int>>& dp){
+        if(i >= coins.size()) return 0;
         if(amount == 0) return 1;
         if(dp[i][amount] != -1) return dp[i][amount];
-        int take = 0 ;
+        int take = 0;
         if(coins[i] <= amount){
-            take = subset(i , amount - coins[i] , coins , dp);
+          take = cntchange(i , amount - coins[i] , coins , dp);
         }
-        int not_take = subset(i+1 , amount , coins , dp);
-        return dp[i][amount] = (take + not_take) ;
+        int not_take = cntchange(i+1 , amount , coins , dp);
+        return dp[i][amount] = take + not_take;
     }
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n+1 , vector<int>(amount + 1 , -1));
-        return subset(0 , amount, coins , dp);
+        vector<vector<int>> dp(coins.size() , vector<int>(amount+1 , -1));
+        return cntchange(0,amount,coins,dp);
     }
 };
