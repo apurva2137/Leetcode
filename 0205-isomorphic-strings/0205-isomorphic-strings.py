c5095de621ecde69a5c37235a99dac0db8212c6c
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-08-24 16:11:14 +0530

    Time: 12 ms (7.74%), Space: 17.9 MB (54.53%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mp1 = defaultdict(int)
@@ -7,12 +6,13 @@ class Solution:
         for i in range(len(s)):
             c = s[i]
             ch = t[i]
+
             if c in mp1 and mp1[c] != ch :
                 return False
-            elif ch in mp2 and mp2[ch] != c:
+            if ch in mp2 and mp2[ch] != c:
                 return False
-
             mp1[c] = ch
             mp2[ch] = c
+
         return True
         
