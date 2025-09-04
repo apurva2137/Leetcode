79f45f0790288bf5f54a6a677493b026ab091774
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-09-05 00:21:52 +0530

    Time: 26 ms (32.64%), Space: 35.7 MB (65.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<int> vis(n,-1);
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    q.push({i,j});
-                }
-                else mat[i][j]=INT_MAX;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 0) q.push({i,j});
+                else grid[i][j] = INT_MAX;
             }
         }
-        int row[4]={1,0,-1,0};
-        int col[4]={0,-1,0,1};
+        int row[4] = {-1 , 0 , 1 , 0};
+        int col[4] = {0 , 1 , 0 , -1};
+
+
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
-            for(int i=0;i<4;i++){
-                int nr=row[i]+r;
-                int nc=col[i]+c;
-                if(nc>=0 && nc<m && nr>=0 && nr<n && mat[nr][nc]> mat[r][c]+1){  
-                    mat[nr][nc]=mat[r][c]+1;
-                    q.push({nr,nc});
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nr = r + row[i];
+                int nc = c + col[i];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                 grid[nr][nc] > grid[r][c]){
+                   grid[nr][nc] = grid[r][c] + 1;
+                   q.push({nr,nc});
                 }
             }
         }
-        return mat;
+        return grid;
     }
 };
