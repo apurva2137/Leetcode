de0af1ca060e77631307ca4268d6bd9d4d89a73c
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-09-05 13:32:01 +0530

    Time: 4 ms (92.42%), Space: 28.7 MB (99.62%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& board) {
-       int m = board.size();
-        int n = board[0].size();
+    int numEnclaves(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
+        int m = grid.size();
+        int n = grid[0].size();
 
         for(int i = 0 ; i < m ; i++){
-            if(board[i][0] == 1) q.push({i,0});
-            if(board[i][n-1] == 1) q.push({i,n-1});
+            if(grid[i][0] == 1) q.push({i,0});
+            if(grid[i][n-1] == 1) q.push({i,n-1});
         }
         for(int j = 0 ; j < n ; j++){
-            if(board[0][j] == 1) q.push({0,j});
-            if(board[m-1][j] == 1) q.push({m-1,j});
+            if(grid[0][j] == 1) q.push({0,j});
+            if(grid[m-1][j] == 1) q.push({m-1,j});
         }
-        int row[4] = {-1, 0 , 1 , 0};
-        int col[4] = {0, -1 , 0 , 1};
+
+        int row[4] = {-1 , 0 , 1 , 0};
+        int col[4] = {0 , 1 , 0 , -1};
 
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
 
-            if(board[r][c]!=1) continue;
-            board[r][c] = 0;
+            grid[r][c] = INT_MAX;
 
             for(int i = 0 ; i < 4 ; i++){
                 int nr = r + row[i];
                 int nc = c + col[i];
-
-                if(nr >= 0 && nr < m &&
-                 nc >= 0 && nc < n && board[nr][nc] ==1 ){
-                    q.push({nr,nc});
+    
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n 
+                   && grid[nr][nc] == 1){
+                      grid[nr][nc] = INT_MAX;
+                      q.push({nr , nc});
                 }
-            } 
+            }
         }
         int count = 0;
+
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j <  n ; j++){
-              if(board[i][j] == 1) count++;
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 1) count++;
             }
         }
         return count;
