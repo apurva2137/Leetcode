1da984e26f159f5192a9c02a215833ad84b20659
Apurva_Raut <158991125+apurva2137@users.noreply.github.com>
2025-09-15 01:23:14 +0530

    Time: 15 ms (33.23%), Space: 35 MB (81.85%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int ship(vector<int>& weights, int minw){
-        int total = 0;
-        int day = 1;
+        int tot = 0;
+        int days = 1;
 
         for(int i = 0 ; i < weights.size() ; i++){
-            if(total + weights[i] > minw){
-                day++;
-                total = 0;
+            if(tot + weights[i] <= minw){
+                tot += weights[i];
             }
-            total += weights[i];
+            else{
+                days++;
+                tot = weights[i];
+            }
         }
-        return day;
-    }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int left =*max_element(weights.begin() ,weights.end());
-        int right =accumulate(weights.begin() , weights.end(),0);
 
-        int ans = -1;
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin() , weights.end());
+        int right = accumulate(weights.begin() , weights.end(),0);
+
+        int ans = right;
 
         while(left <= right){
-            int mid = left + ((right - left)/2);
+            int mid = left + ((right - left) / 2);
+
             if(ship(weights , mid) <= days){
                 ans = mid;
-                right = mid -1;
+                right = mid - 1;
             }
+
             else left = mid + 1;
         }
         return ans;
